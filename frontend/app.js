aa474fcd4df7bea3da6b385b6d9ace49707098c9
Hayk <h.balasanyan93@gmail.com>
2023-09-04 18:01:59 +0400

    first commit

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from "./react-components/App";
+import {Provider} from "react-redux";
+import {store} from "./store";
 
-ReactDOM.render(<><App/></>, document.getElementById('app'));
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('app')
+);
