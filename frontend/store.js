d1a771310f6a77d8dc9c8233a1adfd8059db43cd
Hayk <h.balasanyan93@gmail.com>
2023-09-06 10:21:14 +0400

    first commit

--- frontend/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import {newsReducer} from "./newsAction";
-
-
-export const store = configureStore({
-    reducer: {
-        todo: newsReducer
-    },
-});
